79464b98cdd1e89f20fd510be212a9790e98ba2e
Het1124 <hetjpatel1124@gmail.com>
2026-05-15 11:43:49 +0530

    Assignment 2

--- Assignment_2/serial_code_files/utils.cpp
+++ Assignment_2/serial_code_files/utils.cpp
@@ -1,13 +1,86 @@
 #include <math.h>
 #include "utils.h"
 
-// Problem 01
-void matrix_multiplication(double** m1, double** m2, double** result, int N) {}
+/* -------------------------------
+   Problem 01: Loop permutations
+--------------------------------*/
+void matrix_multiplication(double** A, double** B, double** C, int N, int type)
+{
+    if (type == 1) {
+        for (int i=0;i<N;i++)
+        for (int j=0;j<N;j++)
+        for (int k=0;k<N;k++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+    else if (type == 2) {
+        for (int i=0;i<N;i++)
+        for (int k=0;k<N;k++)
+        for (int j=0;j<N;j++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+    else if (type == 3) {
+        for (int j=0;j<N;j++)
+        for (int i=0;i<N;i++)
+        for (int k=0;k<N;k++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+    else if (type == 4) {
+        for (int j=0;j<N;j++)
+        for (int k=0;k<N;k++)
+        for (int i=0;i<N;i++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+    else if (type == 5) {
+        for (int k=0;k<N;k++)
+        for (int i=0;i<N;i++)
+        for (int j=0;j<N;j++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+    else if (type == 6) {
+        for (int k=0;k<N;k++)
+        for (int j=0;j<N;j++)
+        for (int i=0;i<N;i++)
+            C[i][j] += A[i][k]*B[k][j];
+    }
+}
 
-// Problem 02
-void transpose(double** m, double** mt, int N) {}
+/* -------------------------------
+   Problem 02: Transpose
+--------------------------------*/
+void transpose(double** m, double** mt, int N)
+{
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            mt[j][i] = m[i][j];
+}
 
-void transposed_matrix_multiplication(double** m1, double** m2, double** result, int N) {}
+/* -------------------------------
+   Problem 02: Transposed GEMM
+--------------------------------*/
+void transposed_matrix_multiplication(double** m1, double** mt, double** result, int N)
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
 
-// Problem 03
-void block_matrix_multiplication(double** m1, double** m2, double** result, int B, int N) {}
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                sum += m1[i][k] * mt[j][k];
+            }
+            result[i][j] = sum;
+        }
+    }
+}
+
+/* -------------------------------
+   Problem 03: Blocked GEMM
+--------------------------------*/
+void block_matrix_multiplication(double** m1, double** m2, double** result, int B, int N)
+{
+    for (int ii=0; ii<N; ii+=B)
+    for (int jj=0; jj<N; jj+=B)
+    for (int kk=0; kk<N; kk+=B)
+        for (int i=ii; i<ii+B && i<N; i++)
+        for (int j=jj; j<jj+B && j<N; j++)
+        for (int k=kk; k<kk+B && k<N; k++)
+            result[i][j] += m1[i][k]*m2[k][j];
+}
